a4ec852c1f1facf5602057c8817ecaed12b994c6
Kelechi <kelechi.okolo@gmail.com>
2019-10-04 16:29:01 -0700

    yer

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const org1_depts = [
     {
         name:'accounting',
@@ -31,9 +33,25 @@ const org2_depts = [
 var OpenBullet = document.querySelector('ul.OpenBullet');
 var SquareBullet = document.querySelector('ul.SquareBullet');
 
-function PrintDepts(org1_depts, org2_depts){
-    OpenBullet.style.display = 'block';
-    SquareBullet.style.display = 'block';
+function PrintDepts(depts, parent){
+    if(depts.length > 0) {
+        var ul = document.createElement('ul');
+        parent.appendChild(ul);
+    }
 
-    return org1_depts, org2_depts  
+    for (var i = 0; i < depts.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML = depts[i].name;
+        ul.appendChild(li);
+
+    if (depts[i].children){
+        PrintDepts(depts[i].children, li);
+    } 
+    }
+    
 }
+const org1 = document.getElementById("org1");
+const org2 = document.getElementById("org2");
+
+PrintDepts(org1_depts, org1);
+PrintDepts(org2_depts, org2);
